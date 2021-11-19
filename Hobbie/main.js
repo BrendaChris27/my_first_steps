3a84fa4d28f205128e1c71d5a88ae1230f33e611
Brenda Christine <91447916+BrendaChris27@users.noreply.github.com>
2021-11-19 10:24:05 -0300

    Hobby

---
+++ Hobbie/main.js
@@ -0,0 +1,3 @@
+document.getElementById("titulo").innerHTML = "Olá, sou Brenda Christine"
+document.getElementById("idade").innerHTML = "Tenho 27 anos, moro no estado do Rio de Janeiro."
+document.getElementById("hobby").innerHTML = "Meu maior Hobby que quero apresentar a vocês é lutar Muay Thai. =)"
